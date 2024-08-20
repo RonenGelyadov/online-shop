aa710aa95e39427e96b1559f34bafb6faf954871
Ronen Gelyadov <ronengtv@gmail.com>
2024-08-20 16:03:05 +0300

    added code in sql

--- server/pg.sql
+++ server/pg.sql
@@ -5,4 +5,25 @@ CREATE TABLE products (
 	stock INT,
 	description TEXT,
 	image BYTEA
+  category VARCHAR(255)
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  date DATE NOT NULL,
+  customer VARCHAR(255)
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255)
+);
+
+CREATE TABLE orderProducts (
+  orderID INT,
+  productID INT,
+  quantity INT,
+  PRIMARY KEY (orderID, ProductID),
+  FOREIGN KEY (orderID) REFERENCES orders(id),
+  FOREIGN KEY (productID) REFERENCES products(id)
 );
