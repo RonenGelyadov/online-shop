cc123838d5bd64ffe95733758dcf380753a532b6
Ronen Gelyadov <ronengtv@gmail.com>
2024-08-20 16:39:54 +0300

    start working on products page

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ env.config();
 const port = process.env.PORT;
 const serverPassword = process.env.SERVER_PASS;
 
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
+
 server.use(express.json());
 server.use(express.static("public"));
 server.use(bodyParser.urlencoded({ extended: true }));
@@ -39,8 +49,20 @@ server.post("/login-server", (req, res) => {
   }
 });
 
-server.get("/products", (req, res) => {
-  res.render("products.ejs");
+server.get("/products", async (req, res) => {
+  try {
+    const response = await db.query("SELECT * FROM products");
+    let data = response.rows;
+    if (data.length < 1) {
+      data = null;
+    }
+    res.render("products.ejs", {
+      products: data
+    });
+  } catch (err) {
+    console.log (`Error fetching products: ${err.message}`);
+    res.render("products.ejs");
+  }
 });
 
 server.get("/orders", (req, res) => {
